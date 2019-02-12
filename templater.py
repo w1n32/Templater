eea91defd9bccc88503dff4c03fdd000cb15ae02
ext_slemza <slemza@neoflex.ru>
2019-02-12 17:46:35 +0400

    add function load_config

--- templater.py
+++ templater.py
@@ -2,10 +2,20 @@ import sys
 import configparser
 
 
-def LoadConfig(ConfigFile):
+def load_config(file):
+    print("Loading variables from config...\n")
     conf = configparser.ConfigParser()
-    try:
-        conf.read(ConfigFile)
-    except Exception as err:
-        sys.stdout("We have a problem")
-        sys.stdout(err)
+    conf.read(file)
+    config_dict = {}
+    for section in conf.sections():
+        print("Found section:", section)
+        items_dict = {}
+        for item in conf[section]:
+            print("Found item:", item, conf[section][item])
+            items_dict.update({item:conf[section][item]})
+        config_dict.update({section: items_dict})
+    return config_dict
+
+
+if __name__ == '__main__':
+    print(load_config(sys.argv[1]))
